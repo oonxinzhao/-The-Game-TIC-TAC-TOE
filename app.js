a48a95c121801a8b500ea50dd7c997b1008cacf0
oonxinzhao <104043259+oonxinzhao@users.noreply.github.com>
2022-05-13 17:37:42 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,94 @@
+console.log('tic tac toe');
+
+const gameArea = document.querySelector('.game-area')
+const resetBtn = document.querySelector('.reset-btn')
+const blocks = document.querySelectorAll('.block')
+const winnerMsg = document.querySelector('.winner')
+let countClick = 0
+let player1 = 'O'
+let player2 = 'X'
+
+
+function handleClick(event) {
+  let userSelectedBlock = event.target 
+  countClick ++
+  userSelectedBlock.classList.toggle('taken')
+  if (countClick % 2 === 0) {
+    userSelectedBlock.textContent = player1  
+  } else {
+    userSelectedBlock.textContent = player2
+    }
+  blocks.forEach(checkWinner())
+}
+
+
+function checkLine(index1,index2,index3) {
+  if (blocks[index1].textContent === blocks[index2].textContent && blocks[index2].textContent === blocks[index3].textContent) {
+    return true
+  }
+  return false 
+}
+
+function checkMatch() {
+  for (let index = 0; index < 9; index += 3) {
+    if(checkLine(index, index + 1, index + 2)) {
+      return blocks[index]
+    }
+  }
+  for (let index = 0; index < 3; index ++) {
+    if(checkLine(index, index + 3, index + 6 )) {
+      return blocks[index]
+    }
+  }
+  if (checkLine(2,4,6)) {
+    return blocks[2]
+  }
+  if (checkLine(0,4,8)) {
+    return blocks[0]
+  }
+  return ''
+}
+
+function checkFullBoard() {
+  if (document.querySelectorAll('.taken').length === 9) {
+    return true
+  }
+}
+
+function checkWinner() {
+  let winner = checkMatch().textContent
+  if (winner === 'X') {
+    blocks.forEach(function(block) {
+      block.classList.add('disabled-button')
+    })
+    return winnerMsg.textContent = 'winner is player 2'
+  } else if (winner === 'O') {
+    blocks.forEach(function(block) {
+      block.classList.add('disabled-button')
+    })
+    return winnerMsg.textContent = 'winner is player 1'
+  } else if (checkFullBoard() === true) {
+    return winnerMsg.textContent = 'DRAW'
+  }
+}
+
+function resetGame() {
+  blocks.forEach(function(block) {
+    block.textContent = ''
+    winnerMsg.textContent = ''
+    countClick = 0
+    block.classList.remove('taken')
+    block.classList.remove('disabled-button')
+  })
+}
+
+
+blocks.forEach(function(block) {
+  block.addEventListener('click',handleClick)
+})
+resetBtn.addEventListener('click',resetGame)
+
+
+
+
+
